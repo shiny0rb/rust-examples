5cdbd8afb54f2d4f02c18eccf93ef12b7aab47ee
shiny0rb <teacherrossvn@gmail.com>
2021-03-29 20:59:58 +0700

    yeah boi

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    another_function(5, 6);
     println!("Hello, you wee bastard");
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
